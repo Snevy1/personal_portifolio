95f3458fde0dc7029dc38e1ab6cca942fb1fe186
Snevy1 <simiyunevily@gmail.com>
2023-09-08 22:35:49 +0300

    Finalized most of the stuffs

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-document.querySelector("section").scrollIntoView({
-  behavior: "smooth",
+const menu = document.querySelector(".nav__container");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "block";
+  closeBtn.style.display = "block";
+  menuBtn.style.display = "none";
 });
-console.log("Hello");
+
+closeBtn.addEventListener("click", () => {
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "block";
+});
